262766a0bb5f1957a6fd1d1f6a5ffc5c8ca55fbf
Luckkax <luckkax039@gmail.com>
2021-02-07 03:51:01 -0300

    First commit

---
+++ darkbot-main/Dark/Dark-main/src/darkmenu.js
@@ -0,0 +1,38 @@
+const lolizinhamenu = (prefix) => {
+	return 
+
+            COMANDOS:
+
+
+  𝑪𝑶𝑴𝑨𝑵𝑫𝑶𝑺 𝑫𝑨 𝑳𝑶𝑳𝑰𝒁𝑰𝑵𝑯𝑨:
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔══════════════
+      𝑬 𝒐 𝑳𝒖𝒄𝒌𝒌𝒂𝒙 𝒏𝒆 🥵
+╚══════════════
+}
+
+exports.lolizinhamenu = lolizinhamenu
+
+
+
+
+
+
+
+
